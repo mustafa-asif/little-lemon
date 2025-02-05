66ae98ea34adcff3686fe5d08229cb2dc829e7f7
mustafa <mustafawarind03@gmail.com>
2025-02-05 15:38:03 +0500

    navbar css done for now

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Images from '../images/Images'
+import  '../styles/navbar.css'
 
 
 const Nav = () => {
   return (
     <>
-    <nav>
-      <img src={Images.logo} alt="logo1" />
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Contact</li>
+    <nav className='navbar'>
+      
+      <img className='logo'  src={Images.logo} alt="logo1" />
+      <ul className='nav-links'>
+        <li> <a href=""></a>Home</li>
+        <li><a href=""></a>About</li>
+        <li><a href=""></a>Menu</li>
+        <li><a href=""></a>Reservations</li>
+        <li><a href=""></a>Order Online</li>
+        <li><a href=""></a>Login</li>
       </ul>
     </nav>
     </>
