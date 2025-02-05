c2ed622a1ebe0ec379d324ab4013316bbb536658
mustafa <mustafawarind03@gmail.com>
2025-02-05 17:21:34 +0500

    `Refactor Header component and add restaurant logo and description`

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import Images from '../images/Images'
+import  '../styles/header.css'
+
 
 const Header = () => {
   return (
     <>
-    <header>
-      <h1>header</h1>
+    <header className='header'>
+      <div className='container'>
+
+      <div className='title'>
+        <p>Little Lemon</p>
+        <p className='city'>Chicago</p>
+      </div>
+        <span className='description'>
+        <p>
+          We are family owned Mediterranean restaurant in Chicago
+        </p>
+        </span>
+      </div>
+      <div >
+        <img className='logo'src={Images.restaurant} alt="restaurant" />
+      </div>
+      <div >
+        <button className='book-btn'>Reserve a Table</button>
+      </div>
+
     </header>
     
     </>
--- src/images/Images.jsx
+++ src/images/Images.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const Images={
-  logo : new URL('./logo.PNG', import.meta.url).href
+  logo : new URL('./logo.PNG', import.meta.url).href,
+  restaurant: new URL('./restaurant.jpeg', import.meta.url).href
 }
 
 export default Images
