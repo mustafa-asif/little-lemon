213b3f09c0e1479bce6e5627323aac73a301346d
mustafa <mustafawarind03@gmail.com>
2025-02-17 14:57:16 +0500

    some changes which are not specific

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import Nav from '../components/Nav'
-import Header from '../components/Header'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Nav from '../components/Nav';
+import Header from '../components/Header';
 
 const HomePage = () => {
-  return (
-   <>
-   <nav>
-    <Nav />
-   </nav>
-   <header>
-     <Header />
-   </header>
-   </>
-  )
-}
 
-export default HomePage
+
+  return (
+    <>
+      <nav>
+        <Nav />
+      </nav>
+      <header>
+        <Header />
+      </header>
+     
+    </>
+  );
+};
+
+export default HomePage;
